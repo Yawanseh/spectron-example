f11b03795bd0a38620906d3a7600edb758053095
Laith Younes <laithyounes@Laiths-MacBook-Pro-2.local>
2021-07-16 17:19:12 +0300

    format code

--- test/hooks.js
+++ test/hooks.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
+require('dotenv').config()
 
-import { Application } from 'spectron';
-import { should } from 'chai';
-import electron from 'electron';
+import { Application } from 'spectron'
+import { should } from 'chai'
+import electron from 'electron'
 
 global.before(() => {
-  should();
-});
+  should()
+})
 
 export async function startApp() {
   const app = new Application({
     path: electron,
     path: process.env.APP_PATH
   })
-  return app.start();
+  return app.start()
 }
 
 export async function stopApp(app) {
   if (app && app.isRunning()) {
-    await app.stop();
+    await app.stop()
   }
 }
--- test/login.js
+++ test/login.js
@@ -1,45 +1,45 @@
-import { startApp, stopApp } from './hooks';
-import { homePageSelectors, libraryPageSelectors } from './selectors';
+import { startApp, stopApp } from './hooks'
+import { homePageSelectors, libraryPageSelectors } from './selectors'
 
 describe('Sample Test', () => {
-  let app;
+  let app
 
   beforeEach(async () => {
-    app = await startApp();
-  });
+    app = await startApp()
+  })
 
-  afterEach(async() => {
-    await stopApp(app);
-  });
+  afterEach(async () => {
+    await stopApp(app)
+  })
 
 
-it('should create a new browser', async() => {
+  it('should create a new browser', async () => {
     // click the library menu button
     const libraryButton = await app.client.$(homePageSelectors.libraryMenuButton)
     await libraryButton.click()
 
     // click the add new button
     const addNewButton = await app.client.$(libraryPageSelectors.addNewButton)
-    await addNewButton.click();
+    await addNewButton.click()
 
     // click browser demo button
-    const menu = await app.client.$(libraryPageSelectors.addBrowserButton);
-    const menuItems = await menu.$$('a');
-    await menuItems[2].click();
+    const menu = await app.client.$(libraryPageSelectors.addBrowserButton)
+    const menuItems = await menu.$$('a')
+    await menuItems[2].click()
 
     // fill text input
-    const titleInput = await app.client.$(libraryPageSelectors.browserTitleInput);
-    await titleInput.setValue('test');
+    const titleInput = await app.client.$(libraryPageSelectors.browserTitleInput)
+    await titleInput.setValue('test')
 
     // submit form button
-    const formSaveBrowserButton = await app.client.$(libraryPageSelectors.submitButton);
-    await formSaveBrowserButton.click();
+    const formSaveBrowserButton = await app.client.$(libraryPageSelectors.submitButton)
+    await formSaveBrowserButton.click()
 
     // check if the browser is added
-    const browserListGrid = await app.client.$(libraryPageSelectors.browserGrid);
-    const isGridDisplayed = await browserListGrid.isDisplayed();
+    const browserListGrid = await app.client.$(libraryPageSelectors.browserGrid)
+    const isGridDisplayed = await browserListGrid.isDisplayed()
 
     // check if the list of browsers is shown again
-    isGridDisplayed.should.equal(true);
-  });
-});
+    isGridDisplayed.should.equal(true)
+  })
+})
